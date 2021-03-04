bdc4ca2f1f34bd8b28f535a71da47dcfa2160760
shivamvora <shivampatel27102001@gmail.com>
2021-03-04 15:52:02 +0530

    two controller add and method set

---
+++ Layout/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layout.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult GetList()
+        {
+            return View();
+        }
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
