413e84896da42148a97481705d31076fd5c38817
shivamvora <shivampatel27102001@gmail.com>
2021-03-04 16:57:35 +0530

    common layout done

--- Layout/App_Start/RouteConfig.cs
+++ Layout/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace Layout
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
